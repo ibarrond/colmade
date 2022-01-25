6628cd3471eb26b097af8ab86a661305b2621a0f
Alberto Ibarrondo <ibarrond@eurecom.fr>
2022-01-25 16:40:26 +0100

    MHE standard decryption. Many unsuccessful masking attempts in HE

--- colmade/he/main.go
+++ colmade/he/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"crypto/rand"
+	"fmt"
 	"log"
 	"math/big"
 	"os"
@@ -29,7 +31,7 @@ func main() {
 
 	// Creating encryption parameters from a default params with logN=13, logQP=218
 	//  with a plaintext modulus T=65537
-	paramsDef := bfv.PN13QP218
+	paramsDef := bfv.PN12QP109
 	paramsDef.T = 65537
 	params, err := bfv.NewParametersFromLiteral(paramsDef)
 	if err != nil {
@@ -48,7 +50,7 @@ func main() {
 	// Generate input
 	input := make([]uint64, params.N())
 	for i := range input {
-		input[i] = uint64(i)
+		input[i] = uint64(i) % 2
 	}
 
 	// ENCRYPTION PHASE
@@ -57,7 +59,7 @@ func main() {
 	ctxt := EncryptNewCustom(encryptor, ptxt)
 
 	// MASKED DECRYPTION PHASE
-	ptxt_decr := DecryptMaskedNew(decryptor, prng, ctxt) // Modified Decryption!
+	ptxt_decr := DecryptMaskedNoRNSNew(decryptor, prng, ctxt) // Modified Decryption!
 	res := encoder.DecodeUintNew(ptxt_decr)
 
 	// CHECK RESULT
@@ -86,6 +88,8 @@ func DecryptMaskedNew(decryptor bfv.Decryptor, prng *utils.KeyedPRNG, ct *bfv.Ci
 	// ringQP := params.RingQP()
 	level := utils.MinInt(ct.Level(), pt.Level())
 	pt.Value.Coeffs = pt.Value.Coeffs[:level+1]
+	fmt.Println(" ct/pt Degree: ", ct.Degree(), pt.Degree())
+	fmt.Println(" ct/pt Level: ", ct.Level(), pt.Level())
 
 	// Copy c1 to p, and transform to NTT if not done already
 	if ct.Value[0].IsNTT {
@@ -104,24 +108,25 @@ func DecryptMaskedNew(decryptor bfv.Decryptor, prng *utils.KeyedPRNG, ct *bfv.Ci
 
 		GaussianSampler := ring.NewGaussianSampler(prng, ringQ, params.Sigma(), int(6*params.Sigma()))
 		ei := GaussianSampler.ReadLvlNew(level)
-		// levelP := 0
-		// ringQP.ExtendBasisSmallNormAndCenter(e.Q, levelP, nil, e.P)
-		// // divide by P --> needed?
-		// BaseConverter := ring.NewFastBasisExtender(ringQ, ringP)
-		// BaseConverter.ModDownQPtoQ(levelQ, levelP, ct1QP.Q, ct1QP.P, ct1QP.Q)
 
-		// + d * 2 * ri
+		// + d * 2 * ri / z -->  uniform in [0, 2*q/z)
 		d := new(big.Int).Div(params.QBigInt(), big.NewInt(int64(params.T())))
+		z := uint64(4)
 		UniformSampler := ring.NewUniformSampler(prng, ringQ)
-		ri := UniformSampler.ReadNew()             // sample ri (uniform in R_Q) --> could/should be in R_t, but it doesn't matter
-		ringQ.MulScalarBigintLvl(level, ri, d, ri) // ri * d
+		ri := UniformSampler.ReadNew()             // sample ri(uniform in R_Q)
+		ringQ.Mod(ri, params.T()/z, ri)            // ri quasi uniform in [0, t/z)
+		ringQ.MulScalarBigintLvl(level, ri, d, ri) // ri * d / z
 		ringQ.MulScalarLvl(level, ri, 2, ri)       // ri * d * 2
-		ringQ.AddLvl(level, ei, ri, ri)            // ei + ri * d * 2
-		ringQ.ReduceLvl(level, ri, ri)             // (mod Q)
+
+		ringQ.AddLvl(level, ei, ri, ri) // ei + ri * d * 2
+		ringQ.ReduceLvl(level, ri, ri)  // (mod Q)
 		ringQ.NTTLazyLvl(level, ri, ri)
 
 		// c1 * sk + ei + d * 2 * ri (mod Q) <NTT domain>
 		ringQ.AddLvl(level, pt.Value, ri, pt.Value)
+		ringQ.SubLvl(level, pt.Value, ri, pt.Value)
+		ringQ.NTTLazyLvl(level, ei, ei)
+		ringQ.AddLvl(level, pt.Value, ei, pt.Value)
 
 		// ---------------------------------------------------------------------
 
@@ -224,4 +229,156 @@ func EncryptNewCustom(encryptor bfv.Encryptor, plaintext *bfv.Plaintext) *bfv.Ci
 	ct.Value[1].Coeffs = ct.Value[1].Coeffs[:levelQ+1]
 
 	return ct
+}
+
+// ------------------------------ EXPERIMENTAL ------------------------------ //
+
+// Generates a masking bigInt array for the given ringQ.
+func GenMaskBigIntNew(params bfv.Parameters, z int64) []*big.Int {
+	// Set up parameters
+	Q := params.QBigInt()
+	T := big.NewInt(int64(params.T()))
+	delta := new(big.Int).Div(Q, T)
+	delta_2 := delta.Mul(delta, big.NewInt((2)))
+	T_z := new(big.Int).Div(T, big.NewInt(z))
+
+	//Generate cryptographically strong uniformly random coeffs in [0, T/z)
+	mask_arr := make([]*big.Int, params.N())
+	for i := range mask_arr {
+		n, err := rand.Int(rand.Reader, T_z)
+		if err != nil {
+			panic(err)
+		}
+		mask_arr[i] = n
+	}
+
+	// Rescale mask to be between 0 and Q*2/z
+	for i := range mask_arr {
+		mask_arr[i] = mask_arr[i].Mul(mask_arr[i], delta_2)
+	}
+	return mask_arr
+}
+
+// Generates a masking ring polynomial for the given ringQ.
+func GenMaskPolyNew(params bfv.Parameters, z int64) *ring.Poly {
+	// Generate mask as bigInt array
+	mask_arr := GenMaskBigIntNew(params, z)
+
+	// Convert mask to ring polynomial
+	mask := params.RingQ().NewPoly()
+	params.RingQ().SetCoefficientsBigint(mask_arr, mask)
+
+	return mask
+}
+
+// Decrypt decrypts the ciphertext and write the result in ptOut.
+// The level of the output plaintext is min(ciphertext.Level(), plaintext.Level())
+// Output domain will match plaintext.Value.IsNTT value.
+func DecryptNoNTTNew(decryptor bfv.Decryptor, prng *utils.KeyedPRNG, ct *bfv.Ciphertext) (ptOut *bfv.Plaintext) {
+	pt := bfv.NewPlaintext(decryptor.Params())
+	decr := decryptor.RlweDecryptor()
+	params := decryptor.Params()
+
+	ringQ := params.RingQ()
+
+	// Copy c1 to p, and transform to Non-NTT if not done already
+	if ct.Value[0].IsNTT {
+		ringQ.InvNTT(ct.Value[1], pt.Value)
+	} else {
+		ring.CopyValues(ct.Value[1], pt.Value)
+	}
+
+	// c1 * sk
+	ringQ.NTT(pt.Value, pt.Value)
+	ringQ.MulCoeffsMontgomery(pt.Value, decr.SecretKey().Value.Q, pt.Value)
+	ringQ.InvNTT(pt.Value, pt.Value)
+
+	// ---------------------------------------------------------------------
+	// Our modification:
+	// + ei
+	GaussianSampler := ring.NewGaussianSampler(prng, ringQ, params.Sigma(), int(6*params.Sigma()))
+	ei := GaussianSampler.ReadNew()
+	ringQ.AddNoMod(pt.Value, ei, pt.Value)
+
+	// + d * 2 * ri / z
+	// ringQ.AddNoMod(pt.Value, GenMaskPolyNew(params, 4), pt.Value)
+
+	// ---------------------------------------------------------------------
+
+	// + c0
+	if ct.Value[0].IsNTT {
+		ringQ.InvNTTLazy(ct.Value[0], decr.Pool())
+		ringQ.AddNoMod(pt.Value, decr.Pool(), pt.Value)
+	} else {
+		ringQ.AddNoMod(pt.Value, ct.Value[0], pt.Value)
+	}
+
+	// mod q
+	poly_test := pt.Value.CopyNew()
+	ringQ.Reduce(pt.Value, pt.Value)
+	print(poly_test.Equals(pt.Value), ringQ.Equal(poly_test, pt.Value))
+
+	return pt
+}
+
+// Decryption using BigInt arrays
+func DecryptMaskedNoRNSNew(decryptor bfv.Decryptor, prng *utils.KeyedPRNG, ct *bfv.Ciphertext) *bfv.Plaintext {
+	params := decryptor.Params()
+	ringQ := params.RingQ()
+
+	pt := make([]*big.Int, ringQ.N)
+	c0 := make([]*big.Int, ringQ.N)
+	c1s := make([]*big.Int, ringQ.N)
+	sk := make([]*big.Int, ringQ.N)
+	c1 := make([]*big.Int, ringQ.N)
+	c0_poly := ringQ.NewPoly()
+	c1_poly := ringQ.NewPoly()
+
+	// // c1 * sk  <NTT>
+	// ct.Value[1].Copy(c1s_poly)
+	// ringQ.MulCoeffsMontgomery(c1s_poly, decryptor.RlweDecryptor().SecretKey().Value.Q, c1s_poly)
+	// ringQ.InvNTT(c1s_poly, c1s_poly)
+	// ringQ.Reduce(c1s_poly, c1s_poly) // (mod Q)
+	// ringQ.PolyToBigint(c1s_poly, c1s)
+
+	if ct.Value[0].IsNTT {
+		ringQ.InvNTT(ct.Value[0], c0_poly)
+		ringQ.InvNTT(ct.Value[1], c1_poly)
+	} else {
+		ring.CopyValues(ct.Value[0], c0_poly)
+		ring.CopyValues(ct.Value[1], c1_poly)
+	}
+	ringQ.PolyToBigint(c0_poly, c0)
+	ringQ.PolyToBigint(c1_poly, c1)
+
+	ringQ.PolyToBigint(decryptor.RlweDecryptor().SecretKey().Value.Q, sk)
+
+	// // + ei
+	// ei := make([]*big.Int, ringQ.N)
+	// GaussianSampler := ring.NewGaussianSampler(prng, ringQ, params.Sigma(), int(6*params.Sigma()))
+	// ringQ.PolyToBigint(GaussianSampler.ReadNew(), ei)
+	// for i := range pt {
+	// 	pt[i] = new(big.Int).Add(c1s[i], ei[i])
+	// }
+
+	// + c0
+	for i := range pt {
+		c1s[i] = new(big.Int).Mul(c1[i], sk[i])
+		pt[i] = new(big.Int).Add(c1s[i], c0[i])
+	}
+
+	// // + d * 2 * ri / z -->  uniform in [0, 2*q/z)
+	// mask := GenMaskBigIntNew(params, 4)
+	// for i := range pt {
+	// 	pt[i] = pt[i].Add(pt[i], mask[i])
+	// }
+
+	// mod q
+	for i := range pt {
+		pt[i] = new(big.Int).Mod(pt[i], params.QBigInt())
+	}
+
+	plaintext := bfv.NewPlaintext(params)
+	ringQ.SetCoefficientsBigint(pt, plaintext.Value)
+	return plaintext
 }
--- colmade/mhe/main.go
+++ colmade/mhe/main.go
@@ -1,10 +1,8 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"math/big"
-	"math/bits"
 	"os"
 	"time"
 
@@ -34,35 +32,46 @@ func runTimedParty(f func(), N int) time.Duration {
 	return time.Duration(time.Since(start).Nanoseconds() / int64(N))
 }
 
-type Party struct {
+type Bip_s struct {
+	pk      *rlwe.PublicKey
+	params  bfv.Parameters
+	encoder bfv.Encoder
+	input   []int64
+}
+
+type Gate_s struct {
+	pk      *rlwe.PublicKey
+	params  bfv.Parameters
+	encoder bfv.Encoder
+}
+type Party_s struct {
 	sk        *rlwe.SecretKey
 	ckgShare  *drlwe.CKGShare
 	decryptor bfv.Decryptor
-	input     []uint64
-	res       []uint64
-	// from dbfv_test
-	e2s         *dbfv.E2SProtocol
-	s2e         *dbfv.S2EProtocol
-	publicShare *drlwe.CKSShare
-	secretShare *rlwe.AdditiveShare
+	input     []int64
 }
 
-var elapsedEncryptParty time.Duration
-var elapsedEncryptCloud time.Duration
-var elapsedCKGCloud time.Duration
-var elapsedCKGParty time.Duration
-var elapsedEvalCloud time.Duration
-var elapsedEvalParty time.Duration
-var elapsedDecrParty time.Duration
+type PartyPool struct {
+	parties   []*Party_s
+	params    bfv.Parameters
+	encoder   bfv.Encoder
+	encryptor bfv.Encryptor
+	pk        *rlwe.PublicKey
+	prng      *utils.KeyedPRNG
+	flagMask  bool
+}
 
 func main() {
 
-	// GLOBAL SETUP
-	var err error
-	l := log.New(os.Stderr, "", 0)
+	// ---------------------------- GLOBAL SETUP ---------------------------- //
+	Nparties := 3    // Nnumber of parties
+	flagMask := true // Flag to enable/disable masking
 
-	N := 3 // Default number of parties
-	// NGoRoutine := 1 // Default number of Go routines
+	l := log.New(os.Stderr, "", 0)
+	pnrg, err := utils.NewKeyedPRNG([]byte("idemia"))
+	if err != nil {
+		panic(err)
+	}
 
 	// Creating encryption parameters from a default params with logN=13, logQP=218
 	//  with a plaintext modulus T=65537
@@ -73,256 +82,311 @@ func main() {
 		panic(err)
 	}
 
-	crs, err := utils.NewKeyedPRNG([]byte("idemia"))
-	if err != nil {
-		panic(err)
-	}
+	// ---------------------------- INIT PHASE ------------------------------ //
+	// Create pool of parties
+	PPool := NewPartyPool(params, Nparties, pnrg, flagMask)
+	expRes := PPool.GenInputs()
+	// Create Biometric Identity Provider and Gate
+	Bip := &Bip_s{nil, params, bfv.NewEncoder(params), nil}
+	Gate := &Gate_s{nil, params, bfv.NewEncoder(params)}
 
-	// Create common encoder
-	encoder := bfv.NewEncoder(params)
-	// Create each party, and allocate the memory for all the shares that the protocols will need
-	P := genparties(params, N)
-	// Inputs & expected result -> same for all the parties
-	expRes := genInputs(params, P)
+	// --------------------------- KEYGEN PHASE ----------------------------- //
+	// PRIVATE KEY GENERATION --> local
+	PPool.SecretKeyGen()
+	// PUBLIC KEY GENERATION & DISTRIBUTION
+	// Collective public key generation
+	pkey := PPool.ColPubKeyGen(pnrg)
+	// Distribute public key
+	Bip.pk = pkey
+	Gate.pk = pkey
 
-	// KEYGEN -> Collective public key generation
-	pk := ckgphase(params, crs, P)
-	l.Printf("\tGlobal Setup done (cloud: %s, party: %s)\n",
-		elapsedCKGCloud, elapsedCKGParty)
+	// -------------------------- ENCRYPTION PHASE -------------------------- //
+	// ENCRYPTION PHASE -> Encrypt the input vectors
+	encInputs := PPool.EncInputs()
+	// encRef := Bip.encRef()
 
-	// ENCRYPTION PHASE -> Each party encrypts its input vector
-	encInputs := encPhase(params, P, pk, encoder)
-	///////////////// SEGUIR AQUI!!!! ///////////////////////
+	// -------------------------- EVALUATION PHASE -------------------------- //
+	// Biometric matching? Other kind of evaluation?
 
-	tsk := bfv.NewSecretKey(params)
+	// -------------------------- DECRYPTION PHASE -------------------------- //
+	// COLLECTIVE MASKED DECRYPTION
+	pt := ColDecrPhase(encInputs[0], Gate, PPool)
 
-	params.RingQP().AddLvl(1, 1, P[0].sk.Value, P[1].sk.Value, tsk.Value)
-	params.RingQP().AddLvl(1, 1, tsk.Value, P[2].sk.Value, tsk.Value)
-	//  + P[1].sk.Value + P[2].sk.Value
-
-	// COLLECTIVE MASKED DECRYPTION PHASE
-	res0 := colDecrPhase(encInputs[0], params, P, encoder)
+	res := Gate.encoder.DecodeIntNew(pt)
 
 	// check the result
-	l.Printf("\t%v\n", res0[:16])
+	l.Printf("\t%v\n", res[:16])
 	for i := range expRes {
-		if expRes[i] != res0[i]%2 {
+		if expRes[i] != res[i]%2 {
 			//l.Printf("\t%v\n", expRes)
 			l.Println("\tincorrect")
 			return
 		}
 	}
 	l.Println("\tcorrect")
-	l.Printf("> Finished (total cloud: %s, total party: %s)\n",
-		elapsedCKGCloud+elapsedEncryptCloud+elapsedEvalCloud,
-		elapsedCKGParty+elapsedEncryptParty+elapsedEvalParty+elapsedDecrParty)
 
 }
 
-func encPhase(params bfv.Parameters, P []*Party, pk *rlwe.PublicKey, encoder bfv.Encoder) (encInputs []*bfv.Ciphertext) {
-
-	l := log.New(os.Stderr, "", 0)
-
-	encInputs = make([]*bfv.Ciphertext, len(P))
-	for i := range encInputs {
-		encInputs[i] = bfv.NewCiphertext(params, 1)
+// ------------------------------ PARTY POOL -------------------------------- //
+// Create common elements, each party, and allocate input vector
+func NewPartyPool(params bfv.Parameters, Nparties int, prng *utils.KeyedPRNG, flagMask bool) *PartyPool {
+	Pool := &PartyPool{
+		make([]*Party_s, Nparties),
+		params,
+		bfv.NewEncoder(params),
+		nil, nil, prng, flagMask}
+	for i := range Pool.parties {
+		Pool.parties[i] = &Party_s{nil, nil, nil, make([]int64, params.N())}
 	}
-
-	// Each party encrypts its input vector
-	l.Println("> Encrypt Phase")
-	encryptor := bfv.NewEncryptor(params, pk)
-
-	pt := bfv.NewPlaintext(params)
-	elapsedEncryptParty = runTimedParty(func() {
-		for i, pi := range P {
-			encoder.EncodeUint(pi.input, pt)
-			encryptor.Encrypt(pt, encInputs[i])
-		}
-	}, len(P))
-
-	elapsedEncryptCloud = time.Duration(0)
-	l.Printf("\tdone (cloud: %s, party: %s)\n", elapsedEncryptCloud, elapsedEncryptParty)
-
-	return
+	return Pool
 }
 
-func genparties(params bfv.Parameters, N int) []*Party {
-
-	// Create each party, and allocate the memory for all the shares that the protocols will need
-	P := make([]*Party, N)
-	for i := range P {
-		pi := &Party{}
-		pi.sk = bfv.NewKeyGenerator(params).GenSecretKey()
-		pi.decryptor = bfv.NewDecryptor(params, pi.sk)
-		pi.res = make([]uint64, params.N())
-		P[i] = pi
-		P[i].e2s = dbfv.NewE2SProtocol(params, 3.2)
-		P[i].s2e = dbfv.NewS2EProtocol(params, 3.2)
+// Generate a private secret key for each party
+func (Pool *PartyPool) SecretKeyGen() {
+	for _, pi := range Pool.parties {
+		pi.sk = bfv.NewKeyGenerator(Pool.params).GenSecretKey()
+		pi.decryptor = bfv.NewDecryptor(Pool.params, pi.sk)
 	}
-
-	return P
 }
 
-func genInputs(params bfv.Parameters, P []*Party) (expRes []uint64) {
-	// Expected result is bit 1 when the input is odd, and bit 0 when it is even
-	expRes = make([]uint64, params.N())
+// Generate inputs and expected result for the pool of parties
+func (Pool *PartyPool) GenInputs() (expRes []int64) {
+	// Expected result is bit 1 for odd input, 0 for even input
+	expRes = make([]int64, Pool.params.N())
 	for i := range expRes {
-		expRes[i] = uint64(i % 2)
+		expRes[i] = int64(i % 2)
 	}
 
 	// Each party generates its input vector (which is the same for all the parties)
-	for _, pi := range P {
-		pi.input = make([]uint64, params.N())
+	for _, pi := range Pool.parties {
+		pi.input = make([]int64, Pool.params.N())
 		for i := range pi.input {
-			pi.input[i] = uint64(i)
+			pi.input[i] = int64(i)
 		}
 	}
-
-	return
+	return expRes
 }
 
-func ckgphase(params bfv.Parameters, crs utils.PRNG, P []*Party) *rlwe.PublicKey {
+// Collective public key generation
+func (Pool *PartyPool) ColPubKeyGen(commonRandomString utils.PRNG) *rlwe.PublicKey {
 
-	l := log.New(os.Stderr, "", 0)
-
-	l.Println("> CKG Phase")
-
-	ckg := dbfv.NewCKGProtocol(params) // Public key generation
+	ckg := dbfv.NewCKGProtocol(Pool.params)
 	ckgCombined := ckg.AllocateShares()
-	for _, pi := range P {
+	for _, pi := range Pool.parties {
 		pi.ckgShare = ckg.AllocateShares()
 	}
 
-	crp := ckg.SampleCRP(crs)
+	commonRandomPoly := ckg.SampleCRP(commonRandomString)
 
-	elapsedCKGParty = runTimedParty(func() {
-		for _, pi := range P {
-			ckg.GenShare(pi.sk, crp, pi.ckgShare)
-		}
-	}, len(P))
-
-	pk := bfv.NewPublicKey(params)
-
-	elapsedCKGCloud = runTimed(func() {
-		for _, pi := range P {
-			ckg.AggregateShares(pi.ckgShare, ckgCombined, ckgCombined)
-		}
-		ckg.GenPublicKey(ckgCombined, crp, pk)
-	})
-
-	l.Printf("\tdone (cloud: %s, party: %s)\n", elapsedCKGCloud, elapsedCKGParty)
-
-	return pk
-}
-
-func colDecrPhase(ctxt *bfv.Ciphertext, params bfv.Parameters, P []*Party, encoder bfv.Encoder) (results []uint64) {
-
-	l := log.New(os.Stderr, "", 0)
-
-	// Prepare plaintexts
-	ptxtOutput := make([]*bfv.Plaintext, len(P))
-	for i := range ptxtOutput {
-		ptxtOutput[i] = bfv.NewPlaintext(params)
+	for _, pi := range Pool.parties {
+		ckg.GenShare(pi.sk, commonRandomPoly, pi.ckgShare)
 	}
 
-	// Each party encrypts its share
-	elapsedDecrParty = runTimedParty(func() {
-		for i, pi := range P {
-			pi.decryptor.Decrypt(ctxt, ptxtOutput[i])
-			encoder.DecodeUint(ptxtOutput[i], pi.res)
-		}
-	}, len(P))
+	for _, pi := range Pool.parties {
+		ckg.AggregateShares(pi.ckgShare, ckgCombined, ckgCombined)
+	}
+	Pool.pk = bfv.NewPublicKey(Pool.params)
+	ckg.GenPublicKey(ckgCombined, commonRandomPoly, Pool.pk)
+	Pool.encryptor = bfv.NewEncryptor(Pool.params, Pool.pk)
+	return Pool.pk
+}
 
-	// Timing results
-	l.Println("> Decrypt Phase")
-	l.Printf("\tdone (party: %s)\n", elapsedDecrParty)
+// Encrypt the input vectors
+func (Pool *PartyPool) EncInputs() (encInputs []*bfv.Ciphertext) {
+	// Allocate space for the encrypted input vectors
+	encInputs = make([]*bfv.Ciphertext, len(Pool.parties))
+	for i := range encInputs {
+		encInputs[i] = bfv.NewCiphertext(Pool.params, 1)
+	}
 
+	// Each party encrypts its input vector
+	ptxt := bfv.NewPlaintext(Pool.params)
+	for i, pi := range Pool.parties {
+		Pool.encoder.EncodeInt(pi.input, ptxt)
+		Pool.encryptor.Encrypt(ptxt, encInputs[i])
+	}
 	return
 }
 
-func GenDecrShare(params bfv.Parameters, sk *rlwe.SecretKey, crp drlwe.CKGCRP, shareOut *drlwe.CKGShare) {
+// Collective Decryption Phase
+func ColDecrPhase(ct *bfv.Ciphertext, Gate *Gate_s, Pool *PartyPool) *bfv.Plaintext {
 
-	// Limits for random values
-	// max values = floor(sqrt(plaintext modulus))
-	// maxvalue := uint64(math.Sqrt(float64(params.T())))
+	// The gate extracts c1 and sends it to the parties
+	c0, c1 := Gate.ExtractC0C1(ct)
 
-	// maxvalue := uint64(params.T())
-	// // binary mask upper-bound for the uniform sampling
-	// mask := uint64(1<<bits.Len64(maxvalue) - 1)
+	// Each party 'decrypts' c1 to produce its share
+	c1sShares := Pool.ShareDecrypt(c1)
 
-	// Result masks
-	d := new(big.Int).Div(params.QBigInt(), big.NewInt(int64(params.T())))
+	// The gate aggregates the shares and decrypts the result
+	pt := Gate.AggregateAndDecrypt(c1sShares, c0)
 
-	prng, err := utils.NewKeyedPRNG([]byte("idemia"))
-	if err != nil {
-		panic(err)
-	}
-	maxvalue := uint64(params.T())
-	// binary mask upper-bound for the uniform sampling
-	mask := uint64(1<<bits.Len64(maxvalue) - 1)
-	ri := ring.RandUniform(prng, maxvalue, mask)
-	// r2 := ring.RandUniform(prng, maxvalue, mask)
-	// r3 := ring.RandUniform(prng, maxvalue, mask)
-
-	fmt.Println(d, ri)
-
-	// original
-	// ringQP := ckg.params.RingQP()
-
-	// ckg.gaussianSamplerQ.Read(shareOut.Value.Q)
-	// ringQP.ExtendBasisSmallNormAndCenter(shareOut.Value.Q, ckg.params.PCount()-1, nil, shareOut.Value.P)
-	// levelQ, levelP := ckg.params.QCount()-1, ckg.params.PCount()-1
-	// ringQP.NTTLvl(levelQ, levelP, shareOut.Value, shareOut.Value)
-
-	// ringQP.MulCoeffsMontgomeryAndSubLvl(levelQ, levelP, sk.Value, rlwe.PolyQP(crp), shareOut.Value)
+	return pt
 }
 
-type decryptor struct {
-	params bfv.Parameters
-	ringQ  *ring.Ring
-	pool   *ring.Poly
-	sk     *rlwe.SecretKey
+// Generate the decrypted share on each party given c1
+func (Pool *PartyPool) ShareDecrypt(c1 *bfv.Plaintext) []*bfv.Plaintext {
+
+	// Prepare result
+	c1sShares := make([]*bfv.Plaintext, len(Pool.parties))
+	for i := range Pool.parties {
+		c1sShares[i] = bfv.NewPlaintext(Pool.params)
+	}
+
+	ringQ := Pool.params.RingQ()
+	level := c1.Level()
+	sigma := Pool.params.Sigma()
+
+	for i, pi := range Pool.parties {
+
+		// c1 * sk    <NTT domain>
+		ringQ.MulCoeffsMontgomeryLvl(level, c1.Value, pi.sk.Value.Q, c1sShares[i].Value)
+
+		// + ei
+		GaussianSampler := ring.NewGaussianSampler(Pool.prng, ringQ, sigma, int(6*sigma))
+		ei := GaussianSampler.ReadLvlNew(level)
+
+		// + d / Nparties * 2 * ri   --> add mask
+		if Pool.flagMask {
+			d := new(big.Int).Div(Pool.params.QBigInt(),
+				big.NewInt(int64(Pool.params.T())))
+			// d.Div(d, big.NewInt(int64(len(Pool.parties))*2))
+
+			z := uint64(len(Pool.parties)) * 4
+			UniformSampler := ring.NewUniformSampler(Pool.prng, Pool.params.RingQ())
+
+			ri := UniformSampler.ReadNew()             // sample ri (uniform in R_Q)
+			ringQ.Mod(ri, Pool.params.T()/z, ri)       // ri quasi uniform in [0, T/z)
+			ringQ.MulScalarBigintLvl(level, ri, d, ri) // ri * d
+			ringQ.MulScalarLvl(level, ri, 2, ri)       // ri * d * 2
+			ringQ.AddLvl(level, ei, ri, ei)            // ei + (ri * d * 2) mod Q
+		}
+
+		ringQ.NTTLazyLvl(level, ei, ei)
+
+		// c1 * sk + ei <NTT domain>
+		ringQ.AddLvl(level, c1sShares[i].Value, ei, c1sShares[i].Value)
+	}
+	return c1sShares
+}
+
+// --------------------------------- GATE ----------------------------------- //
+// Separate ciphertext into two polys c0 and c1
+func (Gate *Gate_s) ExtractC0C1(ct *bfv.Ciphertext) (*bfv.Plaintext, *bfv.Plaintext) {
+	c0 := bfv.NewPlaintext(Gate.params)
+	c1 := bfv.NewPlaintext(Gate.params)
+	ringQ := Gate.params.RingQ()
+	level := ct.Level()
+	if ct.Degree() != 1 {
+		panic("ct degree must be set to 1. Please relinearize ct before decrypting")
+	}
+
+	c0.Value.Coeffs = c0.Value.Coeffs[:level+1]
+	c1.Value.Coeffs = c1.Value.Coeffs[:level+1]
+	// Extract and transform to NTT if not done already
+	if ct.Value[0].IsNTT {
+		ring.CopyValuesLvl(level, ct.Value[0], c0.Value)
+		ring.CopyValuesLvl(level, ct.Value[1], c1.Value)
+	} else {
+		ringQ.NTTLazyLvl(level, ct.Value[0], c0.Value)
+		ringQ.NTTLazyLvl(level, ct.Value[1], c1.Value)
+	}
+	return c0, c1
+}
+
+// Aggregate Shares and add c0 to decrypt
+func (Gate *Gate_s) AggregateAndDecrypt(c1sShares []*bfv.Plaintext, c0 *bfv.Plaintext) *bfv.Plaintext {
+	pt := bfv.NewPlaintext(Gate.params)
+	ringQ := Gate.params.RingQ()
+	level := c1sShares[0].Level()
+
+	// Aggregate the shares
+	for _, c1sShare := range c1sShares {
+		ringQ.AddLvl(level, pt.Value, c1sShare.Value, pt.Value) // Σ c1s   <NTT>
+	}
+	// Add c0
+	ringQ.AddLvl(level, pt.Value, c0.Value, pt.Value) // c0 + Σ c1s   <NTT>
+
+	// Mod Q
+	ringQ.ReduceLvl(level, pt.Value, pt.Value)
+
+	// Undo NTT
+	ringQ.InvNTTLvl(level, pt.Value, pt.Value)
+
+	return pt
 }
 
 // Decrypt decrypts the ciphertext and write the result in ptOut.
 // The level of the output plaintext is min(ciphertext.Level(), plaintext.Level())
 // Output domain will match plaintext.Value.IsNTT value.
-func DecryptSb(params rlwe.Parameters, sk *rlwe.SecretKey, ciphertext *bfv.Ciphertext, plaintext *bfv.Plaintext) {
+func DecryptMaskedNew(decryptor bfv.Decryptor, prng *utils.KeyedPRNG, ct *bfv.Ciphertext) (ptOut *bfv.Plaintext) {
+	pt := bfv.NewPlaintext(decryptor.Params())
+	decr := decryptor.RlweDecryptor()
+	params := decryptor.Params()
 
-	decryptor := rlwe.NewDecryptor(params, sk)
 	ringQ := params.RingQ()
+	// ringT := params.RingT()
+	// ringP := params.RingP()
+	// ringQP := params.RingQP()
+	level := utils.MinInt(ct.Level(), pt.Level())
+	pt.Value.Coeffs = pt.Value.Coeffs[:level+1]
 
-	level := utils.MinInt(ciphertext.Level(), plaintext.Level())
-
-	plaintext.Value.Coeffs = plaintext.Value.Coeffs[:level+1]
-
-	if ciphertext.Value[0].IsNTT {
-		ring.CopyValuesLvl(level, ciphertext.Value[ciphertext.Degree()], plaintext.Value)
+	// Copy c1 to p, and transform to NTT if not done already
+	if ct.Value[0].IsNTT {
+		ring.CopyValuesLvl(level, ct.Value[ct.Degree()], pt.Value)
 	} else {
-		ringQ.NTTLazyLvl(level, ciphertext.Value[ciphertext.Degree()], plaintext.Value)
+		ringQ.NTTLazyLvl(level, ct.Value[ct.Degree()], pt.Value) //
 	}
 
-	for i := ciphertext.Degree(); i > 0; i-- {
+	for i := ct.Degree(); i > 0; i-- {
+		// c1 * sk    <NTT domain>
+		ringQ.MulCoeffsMontgomeryLvl(level, pt.Value, decr.SecretKey().Value.Q, pt.Value)
 
-		ringQ.MulCoeffsMontgomeryLvl(level, plaintext.Value, sk.Value.Q, plaintext.Value)
+		// ---------------------------------------------------------------------
+		// Our modification:
+		// + ei
 
-		if !ciphertext.Value[0].IsNTT {
-			ringQ.NTTLazyLvl(level, ciphertext.Value[i-1], decryptor.pool)
-			ringQ.AddLvl(level, plaintext.Value, decryptor.pool, plaintext.Value)
+		GaussianSampler := ring.NewGaussianSampler(prng, ringQ, params.Sigma(), int(6*params.Sigma()))
+		ei := GaussianSampler.ReadLvlNew(level)
+		// levelP := 0
+		// ringQP.ExtendBasisSmallNormAndCenter(e.Q, levelP, nil, e.P)
+		// // divide by P --> needed?
+		// BaseConverter := ring.NewFastBasisExtender(ringQ, ringP)
+		// BaseConverter.ModDownQPtoQ(levelQ, levelP, ct1QP.Q, ct1QP.P, ct1QP.Q)
+
+		// + d * 2 * ri
+		d := new(big.Int).Div(params.QBigInt(), big.NewInt(int64(params.T())))
+		UniformSampler := ring.NewUniformSampler(prng, ringQ)
+		ri := UniformSampler.ReadNew()             // sample ri (uniform in R_Q) --> could/should be in R_t, but it doesn't matter
+		ringQ.MulScalarBigintLvl(level, ri, d, ri) // ri * d
+		ringQ.MulScalarLvl(level, ri, 2, ri)       // ri * d * 2
+		ringQ.AddLvl(level, ei, ri, ri)            // ei + ri * d * 2
+		ringQ.ReduceLvl(level, ri, ri)             // (mod Q)
+		ringQ.NTTLazyLvl(level, ri, ri)
+
+		// c1 * sk + ei + d * 2 * ri (mod Q) <NTT domain>
+		ringQ.AddLvl(level, pt.Value, ri, pt.Value)
+
+		// ---------------------------------------------------------------------
+
+		// + c0       <NTT domain>
+		if !ct.Value[0].IsNTT {
+			ringQ.NTTLazyLvl(level, ct.Value[i-1], decr.Pool())  // c0 -> pool
+			ringQ.AddLvl(level, pt.Value, decr.Pool(), pt.Value) // c0 + c1 * sk <NTT>
 		} else {
-			ringQ.AddLvl(level, plaintext.Value, ciphertext.Value[i-1], plaintext.Value)
+			ringQ.AddLvl(level, pt.Value, ct.Value[i-1], pt.Value)
 		}
-
+		// mod q    <NTT> (to keep coeffs not too big inside loop)
 		if i&7 == 7 {
-			ringQ.ReduceLvl(level, plaintext.Value, plaintext.Value)
+			ringQ.ReduceLvl(level, pt.Value, pt.Value)
 		}
 	}
-
-	if (ciphertext.Degree())&7 != 7 {
-		ringQ.ReduceLvl(level, plaintext.Value, plaintext.Value)
+	// mod q    <NTT> (final reduction)
+	if (ct.Degree())&7 != 7 {
+		ringQ.ReduceLvl(level, pt.Value, pt.Value)
 	}
-
-	if !plaintext.Value.IsNTT {
-		ringQ.InvNTTLvl(level, plaintext.Value, plaintext.Value)
+	// undo NTT
+	if !pt.Value.IsNTT {
+		ringQ.InvNTTLvl(level, pt.Value, pt.Value)
 	}
+	return pt
 }
